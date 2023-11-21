dc8a8a8787a1b0554bc5b794de1b18eed1aa8c37
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-21 16:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 let input= document.querySelector(".input");
+let search= document.getElementByClassName("search");
 function btnClicked(){
-	input.focus();
+	search.active();
 	
 }
